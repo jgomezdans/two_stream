84067d9dee5367fb5b662d3728c45500ac9aeca8
Jose Gomez-Dans <jgomezdans@gmail.com>
2016-08-04 16:43:15 +0100

    This example appears to work as expected. Might need to tweak the solver options a bit, but broadly, it's there.

--- eoldas_machinery.py
+++ eoldas_machinery.py
@@ -10,13 +10,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import UnivariateSpline
 
-from get_albedo import Observations
 from tip_helpers import StandardStateTIP, ObservationOperatorTIP
-from tip_helpers import bernards_prior
-
+from tip_helpers import bernards_prior, retrieve_albedo
+from tip_inversion import single_inversion
 # Define some useful globals
-optimisation_options = {'gtol':1e-12,
-                            'maxcor':600, 'maxiter':1500, "disp":20}
+optimisation_options = {'gtol':1e-6,
+                            'maxcor':50, 'maxiter':1500, "disp":20}
 state_grid = np.arange(1, 366, 8)
 
 state_config = OrderedDict()
@@ -29,41 +28,6 @@ state_config['a_nir'] = eoldas_ng.VARIABLE
 state_config['lai'] = eoldas_ng.VARIABLE
 
 
-def retrieve_albedo ( year, fluxnet_site, albedo_unc, albedo_db="albedo.sql"  ):
-    """
-    A method to retrieve albedo from the practical's albedo database. The method 
-    takes the year, the fluxnet site code, a two element list with the associated
-    uncertainy factor as a percentage (e.g. Pinty's would be given as [0.05, 0.07])
-    for the "good" and "other" quality flags.
-    """
-    obs = Observations ( albedo_db )
-    albedo_data = obs.query( year, fluxnet_site )
-    passer = albedo_data.albedo_qa != 255
-    passer = np.where ( np.logical_or ( albedo_data.bhr_vis < 0.01, 
-                                       albedo_data.bhr_nir < 0.01 ), 
-                                        False, passer )
-    
-    doys = albedo_data.doy[passer]
-    observations = np.c_ [ albedo_data.bhr_vis[passer], 
-                     albedo_data.bhr_nir[passer] ]
-    full_inversions = albedo_data.albedo_qa[passer] == 0
-    backup_inversions = albedo_data.albedo_qa[passer] == 1
-    is_snow = albedo_data.snow_qa == 1
-    mask = np.c_[ doys, doys.astype(np.int)*0 + 1 ]
-    bu = observations*0.0
-    for i in xrange(2):
-        bu[full_inversions, i] = np.max ( np.c_[
-            np.ones_like (doys[full_inversions])*2.5e-3, 
-            observations[full_inversions,i]*albedo_unc[0]], axis=1)
-        bu[backup_inversions, i] = np.max ( np.c_[
-            np.ones_like (doys[backup_inversions])*2.5e-3, 
-            observations[backup_inversions,i]*albedo_unc[1]], axis=1)
-    passer_snow = passer.copy()
-    passer_snow[passer] = is_snow[passer]
-    # Observation uncertainty is 5% and 7% for flags 0 and 1, resp
-    # Min of 2.5e-3
-    # bu = bu*0. + 0.005
-    return observations, mask, bu, passer_snow
 
 
 def tip_inversion ( year, fluxnet_site, albedo_unc=[0.05, 0.07], green_leaves=False,
@@ -118,22 +82,18 @@ def tip_inversion ( year, fluxnet_site, albedo_unc=[0.05, 0.07], green_leaves=Fa
     # Now, we will do the function minimisation with `n_tries` different starting
     # points. We choose the one with the lowest cost...
 
-    ndvi = (obsop.observations[:,1] - obsop.observations[:,0])/\
-           (obsop.observations[:,1] + obsop.observations[:,0])
-    lai_init =  0.26*np.power(26.28, ndvi)*2.5
-    xp = mask[:,0]
-    spl = UnivariateSpline ( xp, 2.*lai_init )
-    lai_init = spl( state_grid )
-    x_dict = OrderedDict()
+
+
+    retval = single_inversion( year, fluxnet_site)
+    x_dict = {}
+    for i,k in enumerate ( ['omega_vis', 'd_vis', 'a_vis', 'omega_nir', 'd_nir', 'a_nir', 'lai']):
+        x_dict[k] = retval[:,i]
+
     results = []
     for i in xrange(n_tries):
-        if i == 0:
-            for p, v in the_state.default_values.iteritems():
-                x_dict[p] = np.ones(len(state_grid))*v
-        else:    
-            for p, v in the_state.default_values.iteritems():
-                x_dict[p] = np.random.rand(len(state_grid))
-            x_dict['lai'] = lai_init
+        if n_tries > 1:
+            x0 = np.random.multivariate_normal( prior.mu, np.array(np.linalg.inv(prior.inv_cov.todense())))
+            x_dict = the_state._unpack_to_dict ( x0 )
         retval = the_state.optimize(x_dict, do_unc=True)
         results.append ( ( the_state.cost_history['global'][-1],
                          retval ) )
@@ -184,18 +144,18 @@ def regularised_tip_inversion ( year, fluxnet_site, regularisation,
 
 if __name__ == "__main__":
     params = ['omega_vis', 'd_vis', 'a_vis', 'omega_nir', 'd_nir', 'a_nir', 'lai']
-    site = "DE-Hai"
-    for year in [2008, 2009, 2010]:
+    site = "SE-Abi"
+    for year in [2008, 2010]:
         retval, state, obs = tip_inversion ( year, site, green_leaves=False)
         fig = plt.figure()
         fig.suptitle("%s (%d)" % (site, year))
 
         for i,p in enumerate ( params ):
             plt.subplot(5,2, i+1 )
-            plt.fill_between ( state.state_grid, retval['real_ci5pc'][p],
-                              retval['real_ci95pc'][p], color="0.8" )
-            plt.fill_between ( state.state_grid, retval['real_ci25pc'][p],
-                              retval['real_ci75pc'][p], color="0.6" )
+            # plt.fill_between ( state.state_grid, retval['real_ci5pc'][p],
+            #                   retval['real_ci95pc'][p], color="0.8" )
+            # plt.fill_between ( state.state_grid, retval['real_ci25pc'][p],
+            #                   retval['real_ci75pc'][p], color="0.6" )
 
             plt.plot ( state.state_grid, retval['real_map'][p] )
             if p in ["d_vis", "d_nir", "lai"]:
